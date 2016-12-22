d0e5804f8f0f64017a5293638104f76cb1f949ea
Vickey Crouch <crouchvickey@gmail.com>
2016-12-21 18:51:56 -0600

    initial commit

---
+++ src/FizzBuzz/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                bool fizz = i % 3 == 0;
+                bool buzz = i % 7 == 0;
+                if (fizz && buzz)
+                    Console.WriteLine("FizzBuzz " + i.ToString());
+                else if (fizz)
+                    Console.WriteLine("Fizz " + i.ToString());
+                else if (buzz)
+                    Console.WriteLine("Buzz " + i.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
